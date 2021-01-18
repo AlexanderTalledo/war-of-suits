63c431181d47b0a9b805013881b5f4f494e1e3cd
Alex Talledo <alexta.dev@gmail.com>
2021-01-18 09:57:42 +0100

    Create application activity

---
+++ app/src/main/java/alexta/interviews/wallapop/wos/application/view/ApplicationActivity.kt
@@ -0,0 +1,12 @@
+package alexta.interviews.wallapop.wos.application.view
+
+import alexta.interviews.wallapop.wos.R
+import alexta.interviews.wallapop.wos.shared.framework.activities.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ApplicationActivity : BaseActivity() {
+
+    override fun layoutId() = R.layout.activity_application
+
+}
