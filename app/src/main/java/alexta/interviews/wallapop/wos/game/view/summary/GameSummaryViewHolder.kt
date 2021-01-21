97a214648c1bad600dbd26b6bb87f72c3f82cab8
Alex Talledo <alexta.dev@gmail.com>
2021-01-21 21:54:51 +0100

    Use ViewBinding on summary ViewHolder

--- app/src/main/java/alexta/interviews/wallapop/wos/game/view/summary/GameSummaryViewHolder.kt
+++ app/src/main/java/alexta/interviews/wallapop/wos/game/view/summary/GameSummaryViewHolder.kt
@@ -1,42 +1,42 @@
 package alexta.interviews.wallapop.wos.game.view.summary
 
-import alexta.interviews.wallapop.wos.R
+import alexta.interviews.wallapop.wos.databinding.ItemGameSummaryBinding
 import android.view.View
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class GameSummaryViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+class GameSummaryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    private val viewBinding = ItemGameSummaryBinding.bind(view)
 
     internal fun bind(item: GameSummaryItemView) {
         setGameSummaryItemView(item)
     }
 
     private fun setGameSummaryItemView(item: GameSummaryItemView) = with(item) {
-        view.findViewById<LinearLayout>(R.id.gameSummaryItemContainer)
-            .setBackgroundResource(backgroundColor)
+        viewBinding.gameSummaryItemContainer.setBackgroundResource(backgroundColor)
         setGameSummaryItemPlayerOneView(playerOneItemView)
         setGameSummaryItemPlayerTwoView(playerTwoItemView)
     }
 
     private fun setGameSummaryItemPlayerOneView(playerOneItemView: GameSummaryItemPlayerView) {
         with(playerOneItemView) {
-            view.findViewById<TextView>(R.id.gameSummaryItemPlayerOneCardRank).setText(cardRank)
-            view.findViewById<ImageView>(R.id.gameSummaryItemPlayerOneCardSuit)
-                .setImageResource(cardSuit)
-            view.findViewById<TextView>(R.id.gameSummaryItemPlayerOneName).text = name
-            view.findViewById<TextView>(R.id.gameSummaryItemPlayerOneScore).text = score
+            with(viewBinding) {
+                gameSummaryItemPlayerOneCardRank.setText(cardRank)
+                gameSummaryItemPlayerOneCardSuit.setImageResource(cardSuit)
+                gameSummaryItemPlayerOneName.text = name
+                gameSummaryItemPlayerOneScore.text = score
+            }
         }
     }
 
     private fun setGameSummaryItemPlayerTwoView(playerTwoItemView: GameSummaryItemPlayerView) {
         with(playerTwoItemView) {
-            view.findViewById<TextView>(R.id.gameSummaryItemPlayerTwoCardRank).setText(cardRank)
-            view.findViewById<ImageView>(R.id.gameSummaryItemPlayerTwoCardSuit)
-                .setImageResource(cardSuit)
-            view.findViewById<TextView>(R.id.gameSummaryItemPlayerTwoName).text = name
-            view.findViewById<TextView>(R.id.gameSummaryItemPlayerTwoScore).text = score
+            with(viewBinding) {
+                gameSummaryItemPlayerTwoCardRank.setText(cardRank)
+                gameSummaryItemPlayerTwoCardSuit.setImageResource(cardSuit)
+                gameSummaryItemPlayerTwoName.text = name
+                gameSummaryItemPlayerTwoScore.text = score
+            }
         }
     }
 
